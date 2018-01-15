852732e2d02180565b6c4e932a6d78ab98a2dcdc
Markus Flodmark <markus.flodmark@gmail.com>
2018-01-15 16:38:37 +0100

    UpdateCart = OK

--- PizzeriaASP/Controllers/AdminCartsController.cs
+++ PizzeriaASP/Controllers/AdminCartsController.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzeriaASP.Models;
+using PizzeriaASP.ViewModels;
+using SQLitePCL;
 
 namespace PizzeriaASP.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminCartsController : Controller
     {
+        private readonly TomasosContext _context;
+
+        public AdminCartsController(TomasosContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = new AdminCartViewModel()
+            {
+                Orders = _context.Bestallning.Include(x => x.Kund).ToList()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCart(int id)
+        {
+            var order = _context.Bestallning.Single(x => x.BestallningId == id);
+
+            foreach (var item in order.BestallningMatratt)
+            {
+                _context.BestallningMatratt.Remove(item);
+            }
+            _context.SaveChanges();
+
+            _context.Remove(order);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateCart(int id)
+        {
+            var order = _context.Bestallning.Single(x => x.BestallningId == id);
+
+            order.Levererad = true;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ PizzeriaASP/ViewModels/AdminCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using PizzeriaASP.Models;
+
+namespace PizzeriaASP.ViewModels
+{
+    public class AdminCartViewModel
+    {
+        public List<Bestallning> Orders { get; set; }
+    }
+}
