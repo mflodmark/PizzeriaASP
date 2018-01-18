83be0a8c5106893b11760537172e71ac8ddb2efd
Markus Flodmark <markus.flodmark@gmail.com>
2018-01-18 14:38:26 +0100

    sticky header

--- PizzeriaASP/wwwroot/JS/layout.js
+++ PizzeriaASP/wwwroot/JS/layout.js
@@ -1,1 +1,14 @@
-﻿
+﻿//$(document).ready(function () {
+//    console.log("test");
+//    $(window).scroll(function () {
+
+//        if ($(this).scrollTop() > 50) {
+//            $("#my-container").addClass("change-opacity");
+//            console.log("scroll");
+//        }
+
+//        if ($(this).scrollTop() < 50) {
+//            $("#my-container").classList.remove("change-opacity");
+//        }
+//    });
+//});
